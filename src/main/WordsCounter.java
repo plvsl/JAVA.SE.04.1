0a19fc720bd2cb22d43b31338b54964b33dc4207
Polina <vslpoulin@gmail.com>
2018-04-08 19:15:01 +0300

    completed

--- src/main/WordsCounter.java
+++ src/main/WordsCounter.java
@@ -9,29 +9,26 @@ import java.util.Map;
 
 class WordsCounter {
 
+    private final static String KEY_WORDS = "private, protected, public, abstract, class, extends, final, implements, interface, " +
+            "native, new, static, strictfp, synchronized, transient, volatile, break, case, continue, default, " +
+            "do, else, for, if, instanceof, return, switch, while, import, package, boolean, byte, char, double, " +
+            "float, int, long, short, assert, catch, finally, throw, throws, try, enum, super, this, void, const, goto";
+
     public void countWordsFromFile(String path) throws IOException {
         String originalFilePath = path;
         File originalFile = new File(originalFilePath);
-        FileInputStream fileInputStream = new FileInputStream(originalFile);
-
-        byte a;
         StringBuilder stringBuilder = new StringBuilder();
-        while ((a = (byte) fileInputStream.read()) != -1) {
-            stringBuilder.append((char) a);
+
+        try (FileInputStream fileInputStream = new FileInputStream(originalFile)) {
+            byte a;
+            while ((a = (byte) fileInputStream.read()) != -1) {
+                stringBuilder.append((char) a);
+            }
         }
 
-        fileInputStream.close();
-
         String[] originalFileDataStringArray = stringBuilder.toString().split(" ");
-
-        String keyWords = "private, protected, public, abstract, class, extends, final, implements, interface, " +
-                "native, new, static, strictfp, synchronized, transient, volatile, break, case, continue, default, " +
-                "do, else, for, if, instanceof, return, switch, while, import, package, boolean, byte, char, double, " +
-                "float, int, long, short, assert, catch, finally, throw, throws, try, enum, super, this, void, const, goto";
-
-
         Map<String, Integer> counterMap = new HashMap();
-        for (String string : keyWords.split(", ")) {
+        for (String string : KEY_WORDS.split(", ")) {
             counterMap.put(string, 0);
         }
 
@@ -43,13 +40,14 @@ class WordsCounter {
             }
         }
 
-        FileOutputStream fileOutputStream = new FileOutputStream("keyWordsCount");
-        for (String string : counterMap.keySet()) {
-            fileOutputStream.write((string + " ").getBytes());
-            fileOutputStream.write(counterMap.get(string).toString().getBytes());
-            fileOutputStream.write("\n".getBytes());
+        try (FileOutputStream fileOutputStream = new FileOutputStream("keyWordsCount")) {
+            for (String string : counterMap.keySet()) {
+                if (!counterMap.get(string).equals(0)) {
+                    fileOutputStream.write((string + " ").getBytes());
+                    fileOutputStream.write(counterMap.get(string).toString().getBytes());
+                    fileOutputStream.write("\n".getBytes());
+                }
+            }
         }
-        fileOutputStream.close();
     }
-
 }
